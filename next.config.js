4a52cc2b715e9e77c69f2a47063c749db7dcd72d
Joe Bocock <joebocock@gmail.com>
2023-10-10 23:46:15 +0100

    Clean up.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/joebocock.com/`
-  basePath = `/joebocock.com`
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export'
